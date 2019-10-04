30495b790f5f250dc552582076b06f3546af762f
d7p4n4 <bercikezs199999999@gmail.com>
2019-10-04 13:41:07 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = "First line" + Environment.NewLine;
+            File.WriteAllText(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName), text);
+            Console.WriteLine(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName));
+            Console.WriteLine(File.Exists(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + "/WriteFile.txt"));
+        }
+    }
+}
